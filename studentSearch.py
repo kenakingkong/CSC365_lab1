8035effd9bb88a032a5a481fddef7807eaa78157
kenakingkong <kenak@DESKTOP-T22M1KF.localdomain>
2019-04-08 17:33:46 -0700

    parsing works!

--- studentSearch.py
+++ studentSearch.py
@@ -1,63 +1,46 @@
 import pandas as pd
 import sys
 
-# put each function
+# switch statement because they don't have one
+def switch(input, students_table):
 
-def main():
-
-    # read in text file into dataframe
-    file = "students.txt";
-    students_table = pd.read_csv(file, sep=" ");
-
-    # command line prompt
-    response = input("Search: ")
-
-    # switch and call each function
-    switch(response) {
-        case "S":
-            #S[tudent]: <lastname> [B[us]]
-            break;
-
-        case "T":
-            #T[eacher]: <lastname>
-            break;
-
-        case "B":
-            # B[us]: <number>
-            break;
-
-        case "G":
-            #G[rade]: <number> [H[eight]|L[ow]]
-            break;
-
-        case "A":
-            #A[verage]: <number>
-
-        case "I":
-            #I[nfo]
-
-        case "Q":
-            #Q[uit]
-            # sys.exit?
-            break;
-
-        default:
-            #print error statement
-            print ("Usage: F[lag]: <input> [F[lag]]")
-            break
-    }
-
+    if (input[0] == "s" or input[0] == "student"):
+        if (len(input) == 3 and (input[2] == "b" or input[2]=="bus")):
+            student(input[1], True, students_table);
+        else:
+            student(input[1],False, students_table);
+    elif (input[0] == "g" or input[0]=="grade"):
+        if (len(input) == 3):
+            if (input[2] == "h" or input[2]=="high"):
+                grade(int(input[1]), "h",students_table);
+            elif (input[2] == 'l' or input[2] == "low"):
+                grade(int(input[1]),'l',students_table);
+        else
+            grade(int(input[1]),"none")
+    elif ((input[0] == "t" or input[0] == "teacher")):
+        teacher(input[1],students_table);
+    elif ((input[0] == "b" or input[0]=="bus")):
+        bus(int(input[1]),students_table);
+    elif (input[0] == 'a' or input[0]=="average"):
+        average(int(input[1]),students_table);
+    elif (input[0]=='i' or input[0]=="info"):
+        info(students_table);
+    elif (input[0] == 'q' or input[0] == 'quit'):
+        sys.exit();
+    else :
+        print ("Usage: F[lag]: <input> [F[lag]]");
 
 
 # String, boolean ->
 # given a student lastname and option for bus, print student name
 # if bus option is set, print student bus, else print grade and classroom
-def student(lastname, bus=false):
-    student_list = students_table[students_table["StLastName"] == lastname]
+def student(lastname, bus, students_table):
+
+    student_list = students_table.loc[students_table["StLastName"] == lastname.upper()]
     if(bus):
-        print(student_list[[StLastName, StFirstName, Bus]])
+        print(student_list[["StLastName", "StFirstName","Bus"]])
     else:
-        print(student_list[[StLastName, StFirstName, Grade, Classroom]])
+        print(student_list[["StLastName", "StFirstName", "Grade", "Classroom"]])
 
 
 # Search entries by teacher last name
@@ -65,10 +48,40 @@ def student(lastname, bus=false):
 def teacher(teacher):
 
     # filters the dataframe by students with teacher last name
-    student_list = students_table[students_table["TLastName"] == teacher]
+    student_list = students_table.loc[students_table["TLastName"] == teacher];
 
     # prints only these two columns
-    print(student_list[[StLastName,StFirstName]])
+    print(student_list[["StLastName","StFirstName"]])
+
+
+# find the bus route with matching number
+# output the student first and last name, grade and Classroom
+def bus(route):
+
+    student_list = students_table.loc[students_table["Bus"]]
+    print(student_list[["SFirstName","SLastName","Grade","Classroom"]])
+
+# find the matching grade (within constraints if given)
+# 
+
+
+
+def main():
+
+    # read in text file into dataframe
+    file = "students.txt";
+    cols = ["StLastName","StFirstName","Grade","Classroom","Bus","GPA","TLastName","TFirstName"];
+    students_table = pd.read_csv(file, sep=",",header=None, names=cols);
+    #print(students_table)
+
+    # command line prompt
+    response = input("Search: ").lower().split();
+
+    if (len(input) < 2 or len(input) > 3):
+        print ("Usage: F[lag]: <input> [F[lag]]");
+    else :
+        switch(response, students_table);
+
 
 if __name__== "__main__":
   main()
